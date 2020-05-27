a358122a0626bcd86c28334a1d5354a0eb18f78c
dreamer <valsa_de_euridice@naver.com>
2020-05-27 11:14:12 +0900

    second commit

--- src/main/java/com/dreamer/sample/DemoApplication.java
+++ src/main/java/com/dreamer/sample/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("GIT TEST");
+		System.out.println("GIT TEST002");
 	}
 
 }
